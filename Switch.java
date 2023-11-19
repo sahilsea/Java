c5c98188c23fca5769f9719f94dfe3ee16158db4
sahilsea <sa9504793957@gamil.com>
2023-11-19 08:32:28 +0530

    Add: Switch.java

---
+++ Switch.java
@@ -0,0 +1,27 @@
+class Switch13 
+{
+	public static void main(String[] args)
+{
+	int x = 0 ;
+	int num = 1;
+	while (x <= 10)
+	{
+	switch(num) {
+	case 1:
+		System.out.println("1");
+		break;
+	case 2:
+		System.out.println("2");
+		break;
+	case 3:
+		System.out.println("3");
+		break;
+	default:
+		System.out.println("Invalid");
+
+	}
+	num++;
+	x++;
+}
+}
+}
