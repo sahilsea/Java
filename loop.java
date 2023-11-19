74efad97ec7899f2c7f2e6d0a8118b319f35080c
sahilsea <sa9504793957@gamil.com>
2023-11-19 09:10:38 +0530

    loop.java

---
+++ loop.java
@@ -0,0 +1,14 @@
+class loop 
+{
+ public static void main(String[] args)
+{
+int a = 1;
+for( ;a<5;a++)
+{
+for (int b = 1; b<a ; b++){
+	System.out.println("Hello Loop in java");
+}
+System.out.println();
+}
+}
+}
